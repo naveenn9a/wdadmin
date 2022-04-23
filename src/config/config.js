82685f52769852399cea3123f2e683c4486afed7
Naveen <naveen.n9a@gmail.com>
2022-04-23 20:10:52 +0530

    updated env

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,5 @@
-export const API = "http://localhost:9000/v1";
-export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MWM4YmExYzcyYzc5MzJmNDA2NjJjYzQiLCJpYXQiOjE2NDA3MTIyMDYsImV4cCI6MTY3MjI2OTgwNiwidHlwZSI6ImFjY2VzcyJ9.iqTyQC3125J5w3awzTRPr6ZAlFnm49k4WdJTukJrZ0I";
-
-
+export const API = "http://api.webdecrypted.com/v1";
+export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MWM4YmExYzcyYzc5MzJmNDA2NjJjYzQiLCJpYXQiOjE2NTA3MjQ4MTEsImV4cCI6MTY1MDcyNjYxMSwidHlwZSI6ImFjY2VzcyJ9.u3eS-Wv077nFkh02ddMBKT1VwaeRcdnHJlp7LM1o2QM";
 
 
 // columns
